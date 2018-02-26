ce047c9f07fcc42412ec77f2672bccea2e4bb16c
kojaniydracon <kojaniydracon@gmail.com>
2018-02-27 00:13:50 +0300

    bracketed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let arr = [];
+    let cutstr = '';
+
+    arr = bracketsConfig.map(function (br) {
+        return br.join('');
+    });
+
+
+    while (str) {
+        if (cutstr !== str) {
+            cutstr = str;
+            for (let i = 0; i < arr.length; i++) {
+
+
+                str = str.replace(arr[i], '');
+
+
+            }
+        } else {
+            break;
+        }
+    }
+
+
+    // console.log(bool);
+    return !str;
+};
